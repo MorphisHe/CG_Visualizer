b78ad7a7802c700ddc5fa3b957a2d7281cf9cd27
Jiang He <jiangh0124@gmail.com>
2019-09-26 05:17:03 -0400

    able to plot with mouse

--- browser/browser.js
+++ browser/browser.js
@@ -1,7 +1,8 @@
-var drawGrid = function(id) {
-    var canvas = document.getElementById(id);
-    var ctx = canvas.getContext('2d');
+var canvas = document.getElementById("grid");
+var ctx = canvas.getContext('2d');
 
+// drawing a grid that fits the browser size fully
+function drawGrid(){
     // resizeing to the height and width of browser
     w  = window.innerWidth;
     h = window.innerHeight;
@@ -9,33 +10,55 @@ var drawGrid = function(id) {
     ctx.canvas.height = window.innerHeight;
     
     // drawing simple grid lines
-    for (x = 0; x <= w; x += 20) {
+    for (x = 0; x <= w; x += 30) {
         ctx.moveTo(x, 0);
         ctx.lineTo(x, h);
-        for (y = 0; y <= h; y += 20) {
+        for (y = 0; y <= h; y += 30) {
             ctx.moveTo(0, y);
             ctx.lineTo(w, y);
         }
     }
+
+    ctx.strokeStyle = "#1E90FF";
     ctx.stroke();
 };
 
 // redrawing grid to fix the max size of browser while resizing
-function resize_canvas(){
-    canvas = document.getElementById("grid");
-    if (canvas.width  < window.innerWidth)
-    {
-        canvas.width  = window.innerWidth;
-    }
-
-    if (canvas.height < window.innerHeight)
-    {
-        canvas.height = window.innerHeight;
-    }
-    drawGrid("grid"); 
+function resizeCanvas(){
+    if (canvas.width  < window.innerWidth) canvas.width  = window.innerWidth;
+    if (canvas.height < window.innerHeight) canvas.height = window.innerHeight;
+    drawGrid(); 
 }
 
-drawGrid("grid"); 
+// catching mouse click: plot a dot on location of mouse
+// NOTE: canvas cordinate (0, 0) = mouse location (10, 10)
+// we need to convert mouse coordinate to canvas coordinate
+function printMousePos(event) {
+    //document.body.textContent = Math.ceil(289);
+    ctx.beginPath();
+    x = event.clientX-10; // position of mouse in horizontal axis
+    y = event.clientY-10; // position of mouse in vertical axis
 
-// adding listener to resize grid line while browser window resizes
-window.addEventListener('resize', resize_canvas, false);
+    // convert x,y to canvas coordinate
+    if (x%30 >= 15) x = Math.ceil((x) / 30) * 30;
+    else x = Math.floor((x) / 30) * 30;
+    if (y%30 >= 15) y = Math.ceil((y) / 30) * 30;
+    else y = Math.floor((y) / 30) * 30;
+
+    // document.body.textContent = event.clientX + " " + x + " " + y + " " + event.clientY;
+    ctx.arc(x, y, 3.5, 0, 2 * Math.PI, true); // x, y, radius
+    ctx.fill();
+}
+
+// function to add listeners
+function initListeners(){
+    // adding listener to resize grid line while browser window resizes
+    window.addEventListener('resize', resizeCanvas, false);
+
+    // adding listener to mouse click: plotting a dot on place clicked
+    document.addEventListener("click", printMousePos);
+}
+
+// starting function calls
+drawGrid();
+initListeners();
