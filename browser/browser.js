31468a208f2b20bb63fa73e3106e46d1849e7d05
Jiang He <jiangh0124@gmail.com>
2019-09-26 03:37:15 -0400

    adding listener to window: redraw grid to fit the max size of browser while resizing

--- browser/browser.js
+++ browser/browser.js
@@ -1,10 +1,14 @@
-var drawGrid = function(w, h, id) {
+var drawGrid = function(id) {
     var canvas = document.getElementById(id);
     var ctx = canvas.getContext('2d');
-    ctx.canvas.width = w;
-    ctx.canvas.height = h;
 
-
+    // resizeing to the height and width of browser
+    w  = window.innerWidth;
+    h = window.innerHeight;
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    
+    // drawing simple grid lines
     for (x = 0; x <= w; x += 20) {
         ctx.moveTo(x, 0);
         ctx.lineTo(x, h);
@@ -14,8 +18,24 @@ var drawGrid = function(w, h, id) {
         }
     }
     ctx.stroke();
-
 };
 
+// redrawing grid to fix the max size of browser while resizing
+function resize_canvas(){
+    canvas = document.getElementById("grid");
+    if (canvas.width  < window.innerWidth)
+    {
+        canvas.width  = window.innerWidth;
+    }
 
-drawGrid(800, 400, "grid");
+    if (canvas.height < window.innerHeight)
+    {
+        canvas.height = window.innerHeight;
+    }
+    drawGrid("grid"); 
+}
+
+drawGrid("grid"); 
+
+// adding listener to resize grid line while browser window resizes
+window.addEventListener('resize', resize_canvas, false);
