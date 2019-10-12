0e1a13a0da101ce5a7e61135cdb574f40f053350
MorphisHe <jiangh0124@gmail.com>
2019-10-12 12:03:40 -0400

    add point transaction now is able to undo back the edges removed when in remove mode

--- browser/transactions/addPoint_Transaction.js
+++ browser/transactions/addPoint_Transaction.js
@@ -1,11 +1,13 @@
-
 /**
- * this is a transaction class for plotting a point
+ * this is a transaction class for plotting a point or removing a point
  */
 class addPoint_Transaction {
 
-    constructor(p){
+    // edgesRemoved will be empty array [] in plotting mode
+    // O.W it will contain edges that is removed when removing a point
+    constructor(p, edgesRemoved) {
         this.p = p;
+        this.edgesRemoved = edgesRemoved;
     }
 
     doTransaction() {
@@ -14,5 +16,13 @@ class addPoint_Transaction {
 
     undoTransaction() {
         plotOrRemove(this.p);
+        if (this.edgesRemoved.length != 0) {
+            // if this is in remove mode
+            // while undoing, add the removed edges back to data then redraw
+            this.edgesRemoved.forEach(edge => {
+                data.addEdge([edge.x1, edge.y1], [edge.x2, edge.y2])
+            });
+            redraw();
+        }
     }
 }
