bfb05e21631b0b042237b7e80557616b5c381ed7
Jiang He <jiangh0124@gmail.com>
2019-10-15 20:03:53 -0400

    new file to test functions in cg tools

---
+++ browser/testing.js
@@ -0,0 +1,23 @@
+// returns 3 points on grid for testing other functions
+function points3(){
+    threePoints = [];
+    count = 0;
+    for (var x in data.allPoints){
+        data.allPoints[x].forEach(y => {
+            threePoints.push([x, y]);
+            count += 1;
+            if (count == 3) return threePoints;
+        })
+    }
+    return threePoints;
+}
+
+// function to test area2
+function testCompute(){
+    threePts = points3();
+    a = threePts[0];
+    b = threePts[1];
+    c = threePts[2];
+    document.write("a: " + a + "    b: " + b + "    c: " + c + "<br>" + "area2: " + area2(a, b, c));
+}
+
