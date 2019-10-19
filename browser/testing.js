21e8547f9582197b4a0636984afc336160b4b163
MorphisHe <jiangh0124@gmail.com>
2019-10-19 11:53:44 -0400

    minor change on function name for clarity

--- browser/testing.js
+++ browser/testing.js
@@ -1,8 +1,8 @@
 // returns 3 points on grid for testing other functions
-function points3(){
+function points3() {
     threePoints = [];
     count = 0;
-    for (var x in data.allPoints){
+    for (var x in data.allPoints) {
         data.allPoints[x].forEach(y => {
             threePoints.push([x, y]);
             count += 1;
@@ -13,11 +13,10 @@ function points3(){
 }
 
 // function to test area2
-function testCompute(){
+function testArea2() {
     threePts = points3();
     a = threePts[0];
     b = threePts[1];
     c = threePts[2];
     document.write("a: " + a + "    b: " + b + "    c: " + c + "<br>" + "area2: " + area2(a, b, c));
 }
-
